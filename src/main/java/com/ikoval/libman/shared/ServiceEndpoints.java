6ec40c392901f4b440543bf5a3b11bf8e77ae2a3
kovalevivan <kovalev.ivan.a@gmail.com>
2018-12-26 14:51:18 +0300

    - build was completly configured

--- src/main/java/com/ikoval/libman/shared/ServiceEndpoints.java
+++
@@ -1,5 +0,0 @@
-package com.ikoval.libman.shared;
-
-public interface ServiceEndpoints {
-
-}
--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -2,13 +2,13 @@ INSERT INTO author VALUES (1, 'Lev Tolstoy');
 INSERT INTO author VALUES (2, 'Alexander Pushkin');
 INSERT INTO author VALUES (3, 'Alexander Griboedov');
 
-INSERT INTO book VALUES(1, 'Voina i mir');
-INSERT INTO book VALUES(2, 'Evgeniy Onegin');
-INSERT INTO book VALUES(3, 'Gore ot uma');
-
 INSERT INTO book_genre VALUES(1,'drama');
 INSERT INTO book_genre VALUES(2,'poema');
 INSERT INTO book_genre VALUES(3,'comedy');
+
+INSERT INTO book VALUES(1,'Voina i mir','Moscow publish',1890,1006, TIMESTAMP '2018-12-24');
+INSERT INTO book VALUES(2, 'Evgeniy Onegin','Moscow publish',1840, 300, TIMESTAMP '2018-12-24');
+INSERT INTO book VALUES(3, 'Gore ot uma','Moscow publish',1850,100, TIMESTAMP '2018-12-24');
 
 INSERT INTO jnd_author_book VALUES(1,1);
 INSERT INTO jnd_author_book VALUES(2,2);
